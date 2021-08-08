30e338defb25a0602bfcbec2a10ed5f9eefce985
vkgpt11 <vikasgupta.net@live.com>
2021-08-08 21:07:55 +0530

    fix styling issue

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma'
+})
+
 export default () => {
     return <div>
-        <StylesProvider>
+        <StylesProvider createGenerateClassName={generateClassName}>
             <BrowserRouter>
-                <Route exact path='/pricing' component={Pricing} />
-                <Route exact path='/' component={Landing} />
+                <Switch>
+                    <Route exact path='/pricing' component={Pricing} />
+                    <Route exact path='/' component={Landing} />
+                </Switch>
             </BrowserRouter>
         </StylesProvider>
     </div>
